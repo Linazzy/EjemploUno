3f333477167832733113134957aedc13caf3ea54
alici <alici@192.168.1.55>
2018-02-26 13:19:30 +0100

    oh vaya... lo modifiqué

--- src/patata/ClasePrimera.java
+++ src/patata/ClasePrimera.java
@@ -2,4 +2,23 @@ package patata;
 
 public class ClasePrimera {
 	private int numUno;
+	private int numTres;
+	public ClasePrimera(int numUno, int numTres) {
+		super();
+		this.numUno = numUno;
+		this.numTres = numTres;
+	}
+	public int getNumUno() {
+		return numUno;
+	}
+	public void setNumUno(int numUno) {
+		this.numUno = numUno;
+	}
+	public int getNumTres() {
+		return numTres;
+	}
+	public void setNumTres(int numTres) {
+		this.numTres = numTres;
+	}
+	
 }
