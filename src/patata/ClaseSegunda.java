8181addaf1a46f2d3b12562f3a4e99455b078083
alici <alici@192.168.1.55>
2018-02-26 13:20:55 +0100

    otro cambio

--- src/patata/ClaseSegunda.java
+++ src/patata/ClaseSegunda.java
@@ -2,4 +2,18 @@ package patata;
 
 public class ClaseSegunda {
 	private int numDos;
+
+	public ClaseSegunda(int numDos) {
+		super();
+		this.numDos = numDos;
+	}
+
+	public int getNumDos() {
+		return numDos;
+	}
+
+	public void setNumDos(int numDos) {
+		this.numDos = numDos;
+	}
+	
 }
